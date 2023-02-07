cab36cdf3c4f310a88b470e567b3df68a84a15ed
Nelson Rincon <nelsonrinconc86@gmail.com>
2023-02-07 17:57:58 -0500

    Suma de numeros en rango

--- 01.py
+++ 01.py
@@ -1,2 +1,18 @@
-print("hola nuevo mundo ")
-print("hola nuevo mundo 222")
+""""
+Diseñar un programa que solicite al usuario un número natural positivo y retorne
+la suma del rango indicada por el usuario, es decir desde sumar los números desde
+cero hasta el número que digito el usuario.
+
+
+"""
+
+print("Digite un Numero entero y positivo ")
+Numero=int(input())
+i=0
+suma=0
+while Numero!=i:
+    i=i+1
+    suma=suma+i
+
+print("La sumatoria del rango es: "+str(suma))
+
